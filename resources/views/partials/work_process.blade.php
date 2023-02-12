a1d358d725877a6aa1cabcd3af3c1aaff92c16b5
AK14 <hooploop14@gmail.com>
2023-02-12 18:59:32 +0000

    added why-us block, added information

---
+++ resources/views/partials/work_process.blade.php
@@ -0,0 +1,48 @@
+<section id="pricing" class="pricing ">
+    <div class="container">
+        <div class="section-title" data-aos="fade-up">
+            <h2>Формы работы</h2>
+            <p>Работа ведётся очно и онлайн</p>
+        </div>
+
+        <div class="row">
+
+            <div class="col-4">
+                <div class="box" data-aos="zoom-in" data-aos-delay="100">
+                    <h3>Консультация</h3>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
+                <div class="box" data-aos="zoom-in">
+                    <h3>Краткосрочная&nbsp;работа по определённому&nbsp;запросу <br> (10&nbsp;встреч)</h3>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+                <div class="box" data-aos="zoom-in" data-aos-delay="100">
+                    <h3>Долгосрочная работа <br>(более&nbsp;10&nbsp;встреч)</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-4">
+                <div class="box" data-aos="zoom-in" data-aos-delay="200">
+                    <h3>Групповая терапия</h3>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
+                <div class="box" data-aos="zoom-in">
+                    <h3>Индивидуальная супервизия</h3>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+                <div class="box" data-aos="zoom-in" data-aos-delay="200">
+                    <h3>Групповая супервизия</h3>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
