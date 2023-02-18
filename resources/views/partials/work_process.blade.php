0e44a2f35976ca7efe96098b38c6469fa000310f
AK14 <hooploop14@gmail.com>
2023-02-18 17:34:47 +0000

    rebuild styles rebuild html

--- resources/views/partials/work_process.blade.php
+++ resources/views/partials/work_process.blade.php
@@ -1,44 +1,42 @@
-<section id="process" class="process section-bg">
+<section id="process" class="process">
     <div class="container">
         <div class="section-title" data-aos="fade-up">
             <h2>Формы работы</h2>
-            <p>Работа ведётся очно и онлайн</p>
             <p>Благодаря совместной психологической работе вы сможете пережить трудный жизненный период, лучше понять свои собственные мотивы и ценности, развить эмоциональную чувствительность к себе и другому, повысить самооценку, улучшить качество собственной жизни, выстроить внутренние психологические опоры.</p>
+            <p>Работа ведётся очно и онлайн</p>
         </div>
 
-        <div class="d-flex justify-content-between p-2">
+        <div class="row mb-2">
 
-            <div class="col-4 m-2 align-self-stretch ">
+            <div class="col-md-4 col-sm-6 p-4">
                 <div class="box" data-aos="zoom-in" data-aos-delay="100">
                     <h3>Консультация</h3>
                 </div>
             </div>
 
-            <div class="col-4 m-2">
+            <div class="col-md-4 col-sm-6 p-4">
                 <div class="box" data-aos="zoom-in">
-                    <h3>Краткосрочная&nbsp;работа по определённому&nbsp;запросу <br> (10&nbsp;встреч)</h3>
+                    <h3>Краткосрочная работа по определённому запросу <br> (10&nbsp;встреч)</h3>
                 </div>
             </div>
 
-            <div class=" col-4 m-2 align-self-stretch ">
+            <div class="col-md-4 col-sm-6 p-4">
                 <div class="box" data-aos="zoom-in" data-aos-delay="100">
                     <h3>Долгосрочная работа <br>(более&nbsp;10&nbsp;встреч)</h3>
                 </div>
             </div>
-        </div>
 
-        <div class="d-flex justify-content-between p-2">
-            <div class="col-4 m-2">
+            <div class="col-md-4 col-sm-6 p-4">
                 <div class="box" data-aos="zoom-in" data-aos-delay="200">
                     <h3>Групповая терапия</h3>
                 </div>
             </div>
-            <div class="col-4 m-2">
+            <div class="col-md-4 col-sm-6 p-4">
                 <div class="box" data-aos="zoom-in">
                     <h3>Индивидуальная супервизия</h3>
                 </div>
             </div>
-            <div class="col-4 m-2">
+            <div class="col-md-4 col-sm-6 p-4">
                 <div class="box" data-aos="zoom-in" data-aos-delay="200">
                     <h3>Групповая супервизия</h3>
                 </div>
