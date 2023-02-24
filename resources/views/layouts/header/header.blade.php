72ed4743e3916b54f0dc0733e73a3f9ad7ccfe84
AK14 <hooploop14@gmail.com>
2023-02-24 19:55:34 +0000

    comment logo added phrase

--- resources/views/layouts/header/header.blade.php
+++ resources/views/layouts/header/header.blade.php
@@ -2,8 +2,9 @@
     <!-- The main logo is shown in mobile version only. The centered nav-logo in nav menu is displayed in desktop view  -->
     <div class="logo d-block d-lg-none">
         <a href="/">
-            <img src="{{URL::asset('/img/minina/logo1.png')}}" alt="" class="img-fluid">
+{{--            <img src="{{URL::asset('/img/minina/logo1.png')}}" alt="" class="img-fluid">--}}
         </a>
+        <div class="text-center">ПСИХОЛОГ ГЕШТАЛЬТ-ТЕРАПЕВТ </div>
     </div>
     @include('.layouts.header.menu')
 </div>
