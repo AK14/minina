5387d0908be01c497c74e4e71ece091fe784720a
AK14 <hooploop14@gmail.com>
2023-02-26 07:10:52 +0000

    rebuild foter data

--- resources/views/layouts/footer/top.blade.php
+++ resources/views/layouts/footer/top.blade.php
@@ -1,25 +1,35 @@
-<div class="footer-top">
+<div class="footer-top py-4">
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-6">
-                <a href="#header" class="scrollto footer-logo"><img src="{{URL::asset('/img/minina/logo1.png')}}" alt=""></a>
-                <h3>Минина</h3>
-                <p>ПСИХОЛОГ ГЕШТАЛЬТ-ТЕРАПЕВТ</p>
+        <div class="row mt-4">
+            <div class="col">
+                <h4>Индивидуальный предприниматель <br> Минина Юлия Николаевна</h4>
+                <div class="social-links">
+                    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                </div>
+                <div class="mt-4 text-center">
+                <h4> Документы </h4>
+                <a href="{{ route('contract') }}" class="link-light" target="_blank" > Договор публичной аферты</a>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="row justify-content-end">
+                    <div class="col-lg-8">
+                        Свидетельство о государственной регистрации 0763161 выдано 31.10.2019 г. Минским горисполкомом <br>
+                        УНП 193335610 <br>
+                        Юридический адрес: г. Минск, ул. Воронянского, 3-2/12 <br>
+                        Телефон: +375298461576 <br>
+                        E-mail: mininanikolaenko-y@mail.ru <br>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row footer-newsletter justify-content-center">
-            <div class="col-lg-6">
-                <form action="" method="post">
-                    <input type="email" name="email" placeholder="Enter your Email"><input type="submit" value="Subscribe">
-                </form>
-            </div>
-        </div>
-        <div class="social-links">
-            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-        </div>
+
+
+
     </div>
 </div>
